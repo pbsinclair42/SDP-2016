5464c32d83fd3b79d894fa1caa790ade7115afac
JFriel <james.friel4@gmail.com>
2016-01-20 13:56:35 +0000

    arduino is a bit broken atm...

--- basicmotors/basicmotors.ino
+++ basicmotors/basicmotors.ino
@@ -2,30 +2,18 @@
 #include "Arduino.h"
 #include <Wire.h>
 
-
-/*
-
-motor layout
-
-    1
-3       4
-    2
-
-*/
-
 int blink_pin = 13;
 int a = 5;
 int b = 10;
 int c = 20;
 void setup()
-{/*
+{
    motorForward(0, 0);
    motorForward(1, 0);
    motorForward(2, 0);
    motorForward(3, 0);
    motorForward(4, 0);
    motorForward(5, 0); 
-   */
    pinMode(blink_pin, OUTPUT);
    SDPsetup();
 
@@ -44,67 +32,58 @@ void motorStop()
 char serial_in_char;
 void loop()
 {
-  motorForward(0, 100);
-  motorForward(1, 100);
-  motorForward(2, 100);
-  motorForward(3, 100);
-  motorForward(4, 100);
-  motorForward(5, 100);
-  helloWorld();
-  delay(1000);
-  /*
    while(Serial.available() > 0)
    {
         serial_in_char = (char) Serial.read();
         Serial.print(serial_in_char);
         
         if (serial_in_char == 'f'){//forward
-            motorForward(2, 100);
+            motorForward(1, 100);
             motorForward(3, 100);
             motorForward(0, 0);
-            motorForward(1, 0);
+            motorForward(2, 0);
         } else if (serial_in_char == 'b'){//backwards
-            motorBackward(2, 100);
+            motorBackward(1, 100);
             motorBackward(3, 100);
             motorForward(0, 0);
-            motorForward(1, 0);
-        }
-        else if (serial_in_char == 'l'){//right
             motorForward(2, 0);
+        }
+        else if (serial_in_char == 'l'){//left
+            motorForward(1, 0);
             motorForward(3, 0);
             motorBackward(0, 100);
-            motorForward(1, 100);
+            motorBackward(2, 100);
         }
-        else if (serial_in_char == 'r'){//left
-            motorForward(2, 0);
+        else if (serial_in_char == 'r'){//right
+            motorForward(1, 0);
             motorForward(3, 0);
             motorForward(0, 100);
-            motorBackward(1, 100);
+            motorForward(2, 100);
         } else if (serial_in_char == 's'){//stop
             motorStop();
-        }else if (serial_in_char == 'd'){//diagonalleft <- test
-            motorForward(1, 100);
-            motorForward(2, 0);
-            motorForward(3, 100);
-            motorBackward(0, 100);
-        }else if (serial_in_char == 'e'){//diagonal right <- test
-            motorBackward(1, 50);
-            motorForward(3, 0);
-            motorForward(0, 100);
-            motorForward(2, 100);
-        }else if (serial_in_char == 'o'){//rotate left <- test
-            motorBackward(0, 100);
-            motorForward(3, 0);
+        }else if (serial_in_char == 'd'){//diagonallift
             motorForward(1, 0);
-            motorForward(2, 0);
-        }else if (serial_in_char == 'p'){//rotate right <-test
+            motorForward(3, 0);
             motorForward(0, 100);
             motorForward(2, 100);
-            motorForward(0, 0);
-            motorForward(2, 0);
+        }else if (serial_in_char == 'e'){//diagonal right
+            motorForward(1, 0);
+            motorForward(3, 0);
+            motorForward(0, 100);
+            motorForward(2, 100);
+        }else if (serial_in_char == 'o'){//rotate left
+            motorForward(1, 0);
+            motorForward(3, 0);
+            motorForward(0, 100);
+            motorForward(2, 100);
+        }else if (serial_in_char == 'p'){//rotate right
+            motorForward(1, 0);
+            motorForward(3, 0);
+            motorForward(0, 100);
+            motorForward(2, 100);
         }
    } 
-  */
+ motorStop();
  /*
  motorForward(0, 100);
  motorForward(1, 100);
