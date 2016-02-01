c3d0250114bb9d03dfdd65d08d675f1fcd0fb22c
pbsinclair42 <bobsmith0030+git2@gmail.com>
2016-02-01 17:39:32 +0000

    Started adding possible high level actions robot can take

---
+++ planning/actions.py
@@ -0,0 +1,24 @@
+from constants import *
+from globalObjects import *
+from moveables import Moveable
+from helperClasses import Point, BallStatus
+from robotAPI import *
+
+
+def moveToObject(target):
+    if not isinstance(target,Moveable):
+        raise TypeError("Moveable expected, " + point.__class__.__name__ + " found")
+    # TODO
+
+
+def moveToPoint(point):
+    if not isinstance(point,Point):
+        raise TypeError("Point expected, " + point.__class__.__name__ + " found")
+    distance = point.distance(me.currentPoint)
+    angle = me.currentPoint.bearing(point) - me.currentRotation
+    # ensure the angle is between -pi and pi
+    if angle < -pi:
+        angle+=2*pi
+    elif angle > pi:
+        angle-=2*pi
+    move(distance, angle)
