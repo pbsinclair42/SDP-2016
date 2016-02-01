c10ec009636b915b48884bcc0c0003b77498dbb2
pbsinclair42 <bobsmith0030+git2@gmail.com>
2016-02-01 17:39:01 +0000

    Fixed print commands

--- planning/robotAPI.py
+++ planning/robotAPI.py
@@ -2,19 +2,19 @@
 def turn(x):
     """Rotates the robot x radians clockwise.  Use negative numbers to rotate anticlockwise.  """
     # TODO: Replace stub with actual code to communicate with robot
-    print("Turning "+abs(x)+"radians "+"clockwise" if x>=0 else "anticlockwise")
+    print("Turning "+str(abs(x))+"radians "+"clockwise" if x>=0 else "anticlockwise")
 
 
 def move(distance, angle):
     """Moves `distance` cm at a direction of `angle` radians"""
     # TODO: Replace stub with actual code to communicate with robot
-    print("Moving "+distance+"cm at an angle of "+x+" radians")
+    print("Moving "+str(distance)+"cm at an angle of "+str(angle)+" radians")
 
 
 def kick(distance):
     """Kicks the ball `distance` cm"""
     # TODO: Replace stub with actual code to communicate with robot
-    print("Kicking ball "+distance+"cm")
+    print("Kicking ball "+str(distance)+"cm")
 
 
 def grab():
