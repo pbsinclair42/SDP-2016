c80f2bbe315392b1f972e18fca920fafcdf10eeb
pbsinclair42 <bobsmith0030+git2@gmail.com>
2016-02-01 15:43:22 +0000

    Added stub for API with robot

---
+++ planning/robotAPI.py
@@ -0,0 +1,29 @@
+
+def turn(x):
+    """Rotates the robot x degrees clockwise.  Use negative numbers to rotate anticlockwise.  """
+    # TODO: Replace stub with actual code to communicate with robot
+    pass
+
+
+def move(distance, angle):
+    """Moves `distance` cm at a direction of `angle`"""
+    # TODO: Replace stub with actual code to communicate with robot
+    pass
+
+
+def kick(distance):
+    """Kicks the ball `distance` cm"""
+    # TODO: Replace stub with actual code to communicate with robot
+    pass
+
+
+def grab():
+    """Attempts to grab the ball"""
+    # TODO: Replace stub with actual code to communicate with robot
+    pass
+
+
+def stop():
+    """Stops all motors"""
+    # TODO: Replace stub with actual code to communicate with robot
+    pass
