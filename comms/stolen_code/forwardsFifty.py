d07d76c34d254e8f76f11814ecf49d47b7f206e3
JFriel <james.friel4@gmail.com>
2016-02-06 19:56:28 +0000

    cleaned up the comms and up. updated basictests.py to reflect the new command set and most importantly created a victory dance

--- comms/stolen_code/forwardsFifty.py
+++
@@ -1,8 +0,0 @@
-from RobotCommunications import RobotCommunications
-import time
-
-
-r = RobotCommunications(debug=True)
-r.moveForwards(100)
-time.sleep(4.5)
-r.stop()
--- comms/stolen_code/test.py
+++
@@ -1,7 +0,0 @@
-from RobotCommunications import RobotCommunications
-import time
-
-r = RobotCommunications(debug=True)
-r.moveStraight(100)
-time.sleep(10)
-r.stop()
