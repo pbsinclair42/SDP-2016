added new sample photos with chessboard for pitch0, fixed calibration (rotation, perpsective transformation, translation, undistortion), also added new HSV values for particular computers

vision/calibrate.py
@@ -5,13 +5,13 @@ import numpy as np
 
 COLS = 640
 ROWS = 480
-pitches = util.read_json("../config/undistort.json")
-#pitches = util.read_json("/config/undistort.json")
+#pitches = util.read_json("../config/undistort.json")
+pitches = util.read_json("../config/undistort_pitch0.json")
 
 def step(frame):
     functions = [
         perspective,
-        #translate,
+        translate,
         undistort,
         warp,
         ]
@@ -27,9 +27,9 @@ def pitch_to_numpy(pitch):
 
     return ret
 
-'''def translate(frame):
-    M = np.float32([[1,0,3],[0,1,0]])
-    return cv2.warpAffine(frame, M, (640,480))'''
+def translate(frame):
+    M = np.float32([[1,0,-6],[0,1,-5]])
+    return cv2.warpAffine(frame, M, (640,480))
 
 def undistort(frame):
 
@@ -39,12 +39,11 @@ def undistort(frame):
                         pitch["new_camera_matrix"])
 
 def warp(frame):
-    M = cv2.getRotationMatrix2D((COLS/2, ROWS/2), 3, 1)
+    M = cv2.getRotationMatrix2D((COLS/2, ROWS/2), 1, 1)
     return cv2.warpAffine(frame, M, (COLS, ROWS))
 
 def perspective(frame):
-
-    pts1 = np.float32([[-5,0],[15,476],[609,474],[627,5]])
+    pts1 = np.float32([[32,10],[43,464],[604,466],[611,15]])
     pts2 = np.float32([[0,0],[0,475],[640,480],[640,0]])
 
     M = cv2.getPerspectiveTransform(pts1,pts2)

vision/test/color_detection.py
@@ -16,28 +16,28 @@ def nothing(x):
 WHITE_LOWER = np.array([1, 0, 100])
 WHITE_HIGHER = np.array([36, 255, 255])
 
-BLUE_LOWER = np.array([100, 130, 130])
-BLUE_HIGHER = np.array([130, 255, 255])
+BLUE_LOWER = np.array([95, 110, 110])
+BLUE_HIGHER = np.array([120, 255, 255])
 
-BRIGHT_BLUE_LOWER = np.array([80, 80, 80])
-BRIGHT_BLUE_HIGHER = np.array([95, 255, 255])
+CYAN_LOWER = np.array([80, 90, 120])
+CYAN_HIGHER = np.array([100, 255, 255])
 
-PINK_LOWER = np.array([145, 130, 130]) 
+PINK_LOWER = np.array([140, 120, 120]) 
 PINK_HIGHER = np.array([170, 255, 255])
 
-RED_LOWER = np.array([0, 150, 150]) 
-RED_HIGHER = np.array([5, 255, 255])
+RED_LOWER = np.array([0, 170, 170]) 
+RED_HIGHER = np.array([4, 255, 255])
 
-MAROON_LOWER = np.array([175, 150, 150]) 
+MAROON_LOWER = np.array([176, 170, 170]) 
 MAROON_HIGHER = np.array([180, 255, 255])
 
 GREEN_LOWER = np.array([60, 110, 110])
 GREEN_HIGHER = np.array([75, 255, 255])
 
-BRIGHT_GREEN_LOWER = np.array([50, 130, 130])
+BRIGHT_GREEN_LOWER = np.array([50, 190, 190])
 BRIGHT_GREEN_HIGHER = np.array([55, 255, 255])
 
-YELLOW_LOWER = np.array([20, 130, 130])
+YELLOW_LOWER = np.array([27, 130, 130])
 YELLOW_HIGHER = np.array([40, 255, 255])
 
 c = Camera()
@@ -53,18 +53,18 @@ while(1):
 	yellow_mask = cv2.inRange(hsv, YELLOW_LOWER, YELLOW_HIGHER)
 	pink_mask = cv2.inRange(hsv, PINK_LOWER, PINK_HIGHER)
 	green_mask = cv2.inRange(hsv, BRIGHT_GREEN_LOWER, BRIGHT_GREEN_HIGHER)
-	bright_blue_mask = cv2.inRange(hsv, BRIGHT_BLUE_LOWER, BRIGHT_BLUE_HIGHER)
+	cyan_mask = cv2.inRange(hsv, CYAN_LOWER, CYAN_HIGHER)
 	blue_mask = cv2.inRange(hsv, BLUE_LOWER, BLUE_HIGHER)
 	red_mask = cv2.inRange(hsv, RED_LOWER, RED_HIGHER)
 	maroon_mask = cv2.inRange(hsv, MAROON_LOWER, MAROON_HIGHER)
 	red = cv2.bitwise_or(red_mask, maroon_mask)
 	
-	cv2.imshow('red', red)
+	cv2.imshow('pink', pink_mask)
 
 	yellow_ret, yellow_thresh = cv2.threshold(yellow_mask,127,255,cv2.THRESH_BINARY)
 	pink_ret, pink_thresh = cv2.threshold(pink_mask,127,255,cv2.THRESH_BINARY)
 	green_ret, green_thresh = cv2.threshold(green_mask,127,255,cv2.THRESH_BINARY)
-	_, bright_blue_thresh = cv2.threshold(bright_blue_mask,127,255,cv2.THRESH_BINARY)
+	_, bright_blue_thresh = cv2.threshold(cyan_mask,127,255,cv2.THRESH_BINARY)
 	red_ret, red_thresh = cv2.threshold(red,127,255,cv2.THRESH_BINARY)
 	blue_ret, blue_thresh = cv2.threshold(blue_mask,127,255,cv2.THRESH_BINARY)
 
@@ -72,7 +72,7 @@ while(1):
 	_, yellow_contours, _ = cv2.findContours(yellow_thresh, 1, 2)
 	_, pink_contours, _ = cv2.findContours(pink_thresh, 1, 2)
 	_, green_contours, _ = cv2.findContours(green_thresh, 1, 2)
-	_, bright_blue_contours, _ = cv2.findContours(bright_blue_thresh, 1, 2)
+	_, cyan_contours, _ = cv2.findContours(bright_blue_thresh, 1, 2)
 	_, red_contours, _ = cv2.findContours(red_thresh, 1, 2)
 	_, blue_contours, _ = cv2.findContours(blue_thresh, 1, 2)
 	
@@ -92,8 +92,8 @@ while(1):
 
 		center = (int(x),int(y))
 		radius = int(radius)
-
-		cv2.circle(frame,center,radius,(0,0,255),2)
+		if radius >= 2:
+			cv2.circle(frame,center,radius,(0,0,255),2)
 
 	for i in range(0, len(blue_contours)):
 		cnt = blue_contours[i]
@@ -109,8 +109,8 @@ while(1):
 
 		center = (int(x),int(y))
 		radius = int(radius)
-
-		cv2.circle(frame,center,7,(255,0,0),2)
+		if radius >= 2:
+			cv2.circle(frame,center,7,(255,0,0),2)
 			
 	for i in range(0, len(pink_contours)):
 		cnt = pink_contours[i]
@@ -127,7 +127,8 @@ while(1):
 		center = (int(x),int(y))
 		pink_balls.append(center)
 		radius = int(radius)
-		cv2.circle(frame,center,radius,(147,20,255),2)
+		if radius >= 2:
+			cv2.circle(frame,center,radius,(147,20,255),2)
 		
 	for i in range(0, len(green_contours)):
 		cnt = green_contours[i]
@@ -144,10 +145,11 @@ while(1):
 		center = (int(x),int(y))
 		green_balls.append(center)
 		radius = int(radius)
-		cv2.circle(frame,center,radius,(0,255,0),2)
+		if radius >= 2:
+			cv2.circle(frame,center,radius,(0,255,0),2)
 	
-	for i in range(0, len(bright_blue_contours)):
-		cnt = bright_blue_contours[i]
+	for i in range(0, len(cyan_contours)):
+		cnt = cyan_contours[i]
 		M = cv2.moments(cnt)
 		if M['m00'] == 0 :
 			continue
@@ -160,7 +162,8 @@ while(1):
 
 		center = (int(x),int(y))
 		radius = int(radius)
-		cv2.circle(frame,center,radius,(255,255,0),2)
+		if radius >= 2:
+			cv2.circle(frame,center,7,(255,255,0),2)
 
 	for i in range(0, len(yellow_contours)):
 		cnt = yellow_contours[i]
@@ -199,17 +202,18 @@ while(1):
 		if num_of_pink == 1:
 			cv2.putText(frame,'OUR',(center[0]-15, center[1]+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 220)
 			cv2.putText(frame,'ATTACKER',(center[0]-30, center[1]+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 220)
-		cv2.circle(frame,center,radius,(0,255,255),2)	
+		if radius >= 2:	
+			cv2.circle(frame,center,radius,(0,255,255),2)	
 
 	
-		
+	cv2.circle(frame,(320,240),5,(255,255,255),2)	
     # Bitwise-AND mask  and original image
 	#res = cv2.bitwise_and(frame,frame, mask= mask)
 	cv2.imshow('hsv', hsv) 
 	cv2.imshow('blurred lines', blur)
 	cv2.imshow('frame',frame)
-	cv2.imwrite('a2.png', frame)
-	cv2.imwrite('b.png', frame)
+	cv2.imwrite('transform.png', frame)
+	#cv2.imwrite('b.png', frame)
 	#cv2.imshow('mask',mask)
 	#cv2.imshow('res',res)
 	

vision/tracker.py
@@ -3,70 +3,150 @@ from calibrate import step
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
-
-# HSV Colors
-WHITE_LOWER = np.array([1, 0, 100])
-WHITE_HIGHER = np.array([36, 255, 255])
-
-BLUE_LOWER = np.array([70, 50, 50])
-BLUE_HIGHER = np.array([160, 255, 255])
-
-RED_LOWER = np.array([0, 175, 90])
-RED_HIGHER = np.array([230, 255, 255])
-
-YELLOW_LOWER = np.array([9, 50, 50])
-YELLOW_HIGHER = np.array([11, 255, 255])
+from colorsHSV import *
 
 c = Camera()
 
-def get_ball_coordinates(ballColor):	
+adjustments = {}
+adjustments['blur'] = (19,19) # needs to be parametrized .. TODO
 
-	# get the frame from video capture
-	frame = step(c.get_frame())
-	# apply Gaussian blurring to remove noise
-	blur = cv2.GaussianBlur(frame,(19,19), 0)
+class Tracker():
 
-	# convert RGB color scale into HSV
-	hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-	
-	# define range of ball color in HSV
-	if ballColor == 'RED':
-		mask = cv2.inRange(hsv, RED_LOWER, RED_HIGHER)
-	elif ballColor == 'BLUE':
-		mask = cv2.inRange(hsv, BLUE_LOWER, BLUE_HIGHER)
+    def get_contours(self, frame, color, adjustments):
 
-	ret,thresh = cv2.threshold(mask,127,255,0)
+        blur_intensity = adjustments['blur']
+        blurred_frame = cv2.GaussianBlur(frame, blur_intensity, 0)
+        hsv_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_frame, color_range[color][0], color_range[color][1])
 
-	_, contours, _ = cv2.findContours(thresh, 1, 2)
+        _, threshold = cv2.threshold(mask, 127, 255, 0)
+        _, contours, _ = cv2.findContours(threshold, 1, 2)
 
-	''' need to check how many objects (balls/noise) are being detected
-		if more than one, probably need to take the coordinates of the bigger contours
-		ttherefore we need to use arclength function'''
+        return contours
 
-	# assume there is only one ball on the pitch and there is no noise:
-	cnt = contours[0]
-	M = cv2.moments(cnt)
+    # Extracts a center from a single contour
+    def get_contour_center(self, contour):
 
-	if M['m00'] == 0:
-		pass
+        (cx, cy), radius = cv2.minEnclosingCircle(contour)
 
-	cx = int(M['m10']/M['m00'])
-	cy = int(M['m01']/M['m00'])
+        return ( int(cx), int(cy) ), int(radius)
 
-	(x,y),radius = cv2.minEnclosingCircle(cnt)
-	center = (int(x),int(y))
-	radius = int(radius)
 
-	return center	
-	
-def opponent_defender_coordinates():
-	pass
+    # Extracts the centers of a list of contours
+    def get_contour_centers(self, contours):
 
-def opponent_attacker_coordinates():
-	pass
+        return [ self.get_contour_center(x) for x in contours ]
 
-def our_defender_coordinates():
-	pass
 
-def our_attacker_coordinates():
-	pass				
+    # Gives us the contour with the biggest area from a list of contours
+    def get_biggest_contour(self, contours):
+
+        max_area = -1
+        max_contour_position = -1
+
+        for i in range(0, len(contours)):
+            area = cv2.contourArea(contours[i])
+            if area > max_area:
+                max_area = area
+                max_contour_position = i
+
+        return contours[max_contour_position]
+
+
+    # Returns the distance between 2 points
+    def distance(self, point_1, point_2):
+
+        dx = ( point_1[0] - point_2[0] )
+        dy = ( point_1[1] - point_2[1] )
+
+        return dx * dx + dy * dy 
+
+
+class BallTracker(Tracker):
+
+    def __init__(self, ballColor):
+        self.color = ballColor
+
+
+    # Extracts the ( center, radius ) of our ball
+    def get_ball_coordinates(self, frame):
+
+        contours = self.get_contours(frame, self.color, adjustments)
+        ball_contour = self.get_biggest_contour(contours)
+
+        return self.get_contour_center(ball_contour)
+
+
+    # Prints the circle around the ball onto the frame
+    def show_ball_frame(self, frame):
+
+        center, radius = self.get_ball_coordinates(frame)
+        cv2.circle(frame, center, (radius+10), (255,255,255), 2)
+        cv2.imshow('test', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+class RobotTracker(Tracker):
+
+    def __init__(self, our_color='yellow', attacker_pink = 1):
+
+        self.side_colors = {}
+        if (our_color == 'yellow'):
+            self.side_colors['us'] = 'yellow'
+            self.side_colors['opponent'] = 'bright_blue'
+        else:
+            self.side_colors['us'] = 'bright_blue'
+            self.side_colors['opponent'] = 'yellow'
+
+        self.num_pink = {}
+        self.num_pink['attacker'] = attacker_pink
+        self.num_pink['defender'] = 4 - attacker_pink
+
+
+    # Gets the coordinates of the robot on a particular SIDE (us or opponent) and for a particular POSITION (attacker or defender)
+    def get_robot_coordinates(self, frame, side, position):
+
+        possible_opponent_contours = self.get_contours(frame, self.side_colors[side], adjustments)
+        pink_contours = self.get_contours(frame, 'pink', adjustments)
+
+        '''print self.get_contour_centers(pink_contours)
+        print ('---------------')
+        print self.get_contour_centers(possible_opponent_contours)'''
+
+        #print len(pink_contours)
+        for contour in possible_opponent_contours:
+            contour_center, radius = self.get_contour_center(contour)
+            pink_contour_count = 0
+
+            for pink_contour in pink_contours:
+                pink_contour_center, _ = self.get_contour_center(pink_contour)
+
+                dist = self.distance( pink_contour_center, contour_center )
+                if dist < 225 :
+                    pink_contour_count += 1
+
+            if pink_contour_count == self.num_pink[position]:
+                return contour_center, radius
+
+        return None
+
+
+    def opponent_defender_coordinates(self, frame):
+        
+        return self.get_robot_coordinates(frame, 'opponent', 'defender')
+
+
+    def opponent_attacker_coordinates(self, frame):
+        
+        return self.get_robot_coordinates(frame, 'opponent', 'attacker')
+
+
+    def our_defender_coordinates(self, frame):
+        
+        return self.get_robot_coordinates(frame, 'us', 'defender')
+
+
+    def our_attacker_coordinates(self, frame):
+        
+        return self.get_robot_coordinates(frame, 'us', 'attacker')   