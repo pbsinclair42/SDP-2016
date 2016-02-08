14f2711192abece1987f844427dfb16cec5cac89
JFriel <james.friel4@gmail.com>
2016-02-08 16:28:54 +0000

    this should be the sending of commands complete

--- comms/RobotCommunications.py
+++ comms/RobotCommunications.py
@@ -12,68 +12,26 @@ class RobotCommunications(Communications):
         super(RobotCommunications, self).__init__(debug)
 
 
-    def holo(self,degrees,distance):#move holonimically
-        self.write("h" + str(degrees) + str(distance) +"256")
+    def holo(self,degrees,distance):
+        self.write(chr(2) + chr(degrees) + chr(distance) +chr(255))
+    def holo2(self,degrees,distance):
+        self.write(chr(130) + chr(degrees) + chr(distance) +chr(255))
     def stop(self):# Stops all motors
-        self.write("s256")
-    def rotate(self,distance,degrees):#rotate X degrees and move y distance
-        self.write("r" +str(distance))
-    def compensate(self,power):#make trajectories
-        self.write("c"+str(power) + "256")
-    def kick(self,distance):#kick
-        self.write("k"+str(distance)+"256")
+        self.write(chr(8)+chr(255)+chr(255)+chr(255))
+    def rotate(self,distance,degrees):
+        self.write(chr(1) +chr(degrees)+chr(distance) + chr(255))
+    def rotate2(self,distance,degrees):
+        self.write(chr(129) +chr(degrees)+chr(distance) + chr(255))
+    def kick(self,distance):
+        self.write(chr(4)+chr(distance)+chr(255)+chr(255))
     def flush(self):#stop previous command
-        self.write("256")
+        self.write(chr(255)+chr(255)+chr(255)+chr(255))
     def grab(self):#grab
-        self.write("g")
+        self.write((chr(16) + chr(255)+chr(255)+chr(255)))
+    def ungrab(self):
+        self.write(chr(32) + chr(255)+chr(255)+chr(255))
+    def flush(self):
+        self.write(chr(64) + chr(255)+chr(255)+chr(255))
     def test(self, argument):#test comms
         print 'I got your message: ' + str(argument)
 
-    """# Straight movement
-    def moveStraight(self, motorPower):
-        self.write("f");# + str(motorPower))
-
-    def moveBackwards(self, motorPower):
-        self.write("b")# + str(motorPower);
-
-    #move left
-    def moveLeft(self,motorPower):
-        self.write("l")# + str(motorPower))
-
-    #move right
-    def moveRight(self,motorPower):
-        self.write("r")#+str(motorPower))
-
-    # Same as Sideways, it's just diagonal
-    def moveDiagonalLeft(self, motorPower):
-        self.write("d")# + str(motorPower))
-
-    def moveDiagonalRight(self, motorPower):
-        self.write("e")# + str(motorPower))
-
-    def rotateLeft(self, motorPower):
-        self.write("o")# + str(motorPower))
-
-    def rotateRight(self, motorPower):
-        self.write("p")# + str(motorPower))
-
-    # Rotate and Grab - 2 args: power_rotate, power_grab
-    def rotateAndGrab(self, motorPower_r, motorPower_g):
-        self.write("ROTATE_GRAB " + str(motorPower_r) + " " + str(motorPower_g))
-
-    def stopRotate(self, motorPower):
-        self.write("STOP_ROTATE " + str(motorPower))
-
-    # Grab and Kick take motorPower. The values should be predefined depending
-    # on how far we need to kick or grab (it will probably be a constant)
-    def grab(self, motorPower):
-        self.write("ACTION GRAB " + str(motorPower))
-
-    def grab_cont(self, motorPower):
-        self.write("ACTION GRAB_CONT " + str(motorPower))
-
-    def kick(self, motorPower):
-        self.write("ACTION KICK " + str(motorPower))
-
-
-    """
