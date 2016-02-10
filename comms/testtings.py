5b0761b13634f62b72843b82a149a306b29ade89
kazzacarrot <kazzacarrot@hotmail.co.uk>
2016-02-10 04:48:14 +0000

    update testtings - now you can set the distence as awell as the angle of rotation

--- comms/testtings.py
+++ comms/testtings.py
@@ -4,14 +4,15 @@ import time
 r = RobotCommunications()
 
 
-def rot_180(n):
-    if 360 % n == 0:
-        for i in range(0, 360 / n):
-            r.rotate(0, n)
+def rot_180(a, d):
+    if 360 % a == 0:
+        for i in range(0, 360 / a):
+            r.rotate(d, a)
             time.sleep(2)
     else:
         print("error")
 
 while True:
-    n = int(input(">>"))
-    rot_180(n)
+    a = int(input("angle:"))
+    d = int(input("distence:"))
+    rot_180(a, d)
