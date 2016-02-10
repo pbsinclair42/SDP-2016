dc8f314114e4914f89cfa930561bffc921a65224
kazzacarrot <kazzacarrot@hotmail.co.uk>
2016-02-10 05:24:30 +0000

    create draft

---
+++ Tests/moveAndCheckVision.py
@@ -0,0 +1,11 @@
+## Get inital robot position from vision
+
+## Calculate where the robot should be after it has moved
+
+## Move the robot
+
+## Once robot has stopped use vision to find the robots possition
+
+## the expected and actual are the same, then return yes! else no.
+
+
