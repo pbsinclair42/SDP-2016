f99810f2644a83e27d5978ccdcbc30ae20abf5b9
Krassy-Gochev <krassy.gochev@gmail.com>
2016-02-26 15:57:12 +0000

    Add workable thread-timing example

--- comms/thread_test.py
+++ comms/thread_test.py
@@ -1,10 +1,7 @@
-from multiprocessing import Process, Pipe
+"""from multiprocessing import Process, Pipe
 import random
 import time
-# 1. 
-
 def worker(conn):
-    """thread worker function"""
     start = time.time()
     while True:
     	if conn.poll():
@@ -26,4 +23,38 @@ if __name__ == '__main__':
     parent_conn.send(l)
     p.join()
 
-        
+        """
+
+import multiprocessing
+import time
+
+def wait_for_event(e):
+    """Wait for the event to be set before doing anything"""
+    print 'wait_for_event: starting'
+    e.wait()
+    print 'wait_for_event: e.is_set()->', e.is_set()
+
+def wait_for_event_timeout(e, t):
+    """Wait t seconds and then timeout"""
+    print 'wait_for_event_timeout: starting'
+    e.wait(t)
+    print 'wait_for_event_timeout: e.is_set()->', e.is_set()
+
+
+if __name__ == '__main__':
+    e = multiprocessing.Event()
+    w1 = multiprocessing.Process(name='block', 
+                                 target=wait_for_event,
+                                 args=(e,))
+    w1.start()
+
+    w2 = multiprocessing.Process(name='non-block', 
+                                 target=wait_for_event_timeout, 
+                                 args=(e, 2))
+    w2.start()
+
+    print 'main: waiting before calling Event.set()'
+    time.sleep(3)
+    print 'main: slept'
+    e.set()
+    print 'main: event is set'
