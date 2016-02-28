e823de712b9b536a70ee690a22661a0315301386
Krassy-Gochev <krassy.gochev@gmail.com>
2016-02-28 14:11:08 +0000

    Ensure backwards compatibility of old Communications due to ascii-valued writing from arduino

--- comms/Communications.py
+++ comms/Communications.py
@@ -23,26 +23,27 @@ class Communications(object):
 
     def write(self, command):
         self.port.write(command + '')
-        #time.sleep(0.005)
-        out = ''
+        time.sleep(0.5)
+        out = []
         while self.port.inWaiting() >0:
-            out += self.port.read(1)
-        if ord(out[-1]) == 111:
-            self.current_cmd += 1
-            print("CMD_DONE")
-        elif out =="CMD_RESEND":#resend
-            print("RESEND")
-            write(command)
-        elif out =="CMD_FULL":#full
-            print("FULL")
-            time.sleep(0.005)
-            write(command)
-        elif out == "CMD_ERROR":#error
-            print("ERROR")
-            write(command)
-        elif out != '':
-            print ">>" + out
-            return out
+            out += ord(self.port.read(1))
+            if ord(out[-1]) == 111:
+                self.current_cmd += 1
+                print("CMD_DONE")
+        #
+        #elif out =="CMD_RESEND":#resend
+        #    print("RESEND")
+        #    write(command)
+        #elif out =="CMD_FULL":#full
+        #    print("FULL")
+        #    time.sleep(0.005)
+        #    write(command)
+        #elif out == "CMD_ERROR":#error
+        #    print("ERROR")
+        #    write(command)
+        #elif out != '':
+        #    print ">>" + out
+        #    return out
             #might want to return this so the planner can use it
-        if self.debug:
-            print self.port.readline()
+        #if self.debug:
+        #    print self.port.readline()
