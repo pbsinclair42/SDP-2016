00d4475a9c31670dd35912419a6c32d217bc759e
Krassy-Gochev <krassy.gochev@gmail.com>
2016-02-29 05:07:55 +0000

    Integrate customized sensor library

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include <Math.h>
 #include <Wire.h>
-
+#include "Accelerometer_Compass_LSM303D.h"
 /***
 IMPORTANT: PLEASE READ BEFORE EDITING:
 
@@ -65,7 +65,6 @@ IMPORTANT: PLEASE READ BEFORE EDITING:
 #define IDLE_STATE 0
 
 // *** Globals ***
-
 // A finite state machine is required to provide concurrency between loop and 
 // serialEvent functions which both support reading serial while moving and
 // rotary-encoder-based motion
@@ -102,6 +101,43 @@ int motion_target;
 int holono_target;
 
 
+// Accelerometer/Compass values
+int accel[3];               // we'll store the raw acceleration values here
+int mag[3];                 // raw magnetometer values stored here
+float realAccel[3];         // calculated acceleration values here
+float heading, titleHeading;
+
+/*  Sensor algorithm
+    Lsm303d.getAccel(accel);
+    
+    while(!Lsm303d.isMagReady());// wait for the magnetometer readings to be ready
+    Lsm303d.getMag(mag);  // get the magnetometer values, store them in mag
+    
+    for (int i=0; i<3; i++)
+    {
+        realAccel[i] = accel[i] / pow(2, 15) * ACCELE_SCALE;  // calculate real acceleration values, in units of g
+    }
+    heading = Lsm303d.getHeading(mag);
+    titleHeading = Lsm303d.getTiltHeading(mag, realAccel);
+    
+    printValues();
+
+    Serial.println("Acceleration of X,Y,Z is");
+    for (int i=0; i<3; i++)
+    {
+        Serial.print(realAccel[i]);
+        Serial.println("g");
+    }
+    // print both the level, and tilt-compensated headings below to compare /
+    Serial.println("The clockwise angle between the magnetic north and x-axis: ");
+    Serial.print(heading, 3); // this only works if the sensor is level
+    Serial.println(" degrees");
+    Serial.print("The clockwise angle between the magnetic north and the projection");
+    Serial.println(" of the positive x-axis in the horizontal plane: ");
+    Serial.print(titleHeading, 3);  // see how awesome tilt compensation is?!
+    Serial.println(" degrees");
+
+*/
 // Main Functions: Setup, Loop and SerialEvent
 void setup() {
     SDPsetup();
@@ -116,6 +152,16 @@ void setup() {
     rotMoveGrabMode = 0;
     bufferOverflow = 0;
     commandOverflow = 0;
+
+    // initialize Accelerometer/Compass sensor
+    char init = 0;
+    init = Lsm303d.initI2C();
+    if (init == 0){
+        Serial.print("Sensor is found");
+    }
+
+
+
     /* Custom commands can be initialized below */
     //for (int i = 0; i < 6; i ++)
     //    motorForward(i, 255);
