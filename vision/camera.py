c18483ffc682fd85f9860ca0940112e39fd09f7a
DVitonyte <dovile.vitonyte@gmail.com>
2016-03-05 17:16:51 +0000

    solving merge issues

--- vision/camera.py
+++ vision/camera.py
@@ -1,30 +1,15 @@
 import cv2
 import numpy as np
 from calibrate import *
-from socket import gethostname
-
-computer_name = gethostname().split('.')[0]
-print "Adjusting settings for: " + computer_name
-properties={'POS_MSEC' : 0, 
-    'POS_FRAMES' : 1,
-    'FRAME_WIDTH' : 3, 
-    'FRAME_HEIGHT' : 4, 
-    'PROP_FPS' : 5,      
-    'PROP_MODE' : 9, 
-    'BRIGHTNESS' : 10,  
-    'CONTRAST' : 11,
-    'COLOR' : 12, 
-    'HUE' : 13
-}
 
 class Camera(object):
     """
     Camera access wrapper.
     """
 
-    def __init__(self, port=0, pitch=0):
+    def __init__(self, pitch=0, port=0):
         self.capture = cv2.VideoCapture(port)         
-            
+        self.pitch = pitch    
 
     def get_frame(self, radial_dist=0):
         """
@@ -33,7 +18,8 @@ class Camera(object):
         Returns the frame if available, otherwise returns None.
         """
         status, frame = self.capture.read()
-        frame = step(frame)
+        #frame = cv2.imread('pitch.png')
+        frame = step(frame, self.pitch)
         return frame
 
     def get_frame_hack(self, radial_dist=0):
