d3010872ec1937662a50b731c1091f10dd8382d5
Krassy-Gochev <krassy.gochev@gmail.com>
2016-03-08 12:10:21 +0000

    Rework comms protocol

--- comms/CommsAPITest.py
+++ comms/CommsAPITest.py
@@ -73,6 +73,6 @@ if __name__ == "__main__":
     """
     while True:
         if commsSystem.am_i_done():
-            move(0, -45);
-            time.sleep(5)
-            print "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+            
+            deg = random.random() * 90
+            turn(90)
