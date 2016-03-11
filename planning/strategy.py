82acaa61c1a9aec1b8ec95e5637780ca0a9d6692
kazzacarrot <kazzacarrot@hotmail.co.uk>
2016-03-11 20:58:48 +0000

    adapt questionable if conditions and code review

--- planning/strategy.py
+++ planning/strategy.py
@@ -2,16 +2,33 @@ from robotAI import *
 
 
 def playBall():
-    if(ball.distance(me) > (ball.distance(ally) and ball.distance(enemies[0]) and ball.distance(enemies[1]))):#if we are closest to ball
+    # if we are closest to ball
+    ballNearerMeThanAlly = ball.distance(me) > ball.distance(ally)
+    ballToEnemyDist = min(ball.distance(enemies[0]), ball.distance(enemies[1]))
+    ballNotNearEnemy = ballToEnemyDist < 50
+    heldByEnemyA = ball.status == BallStatus.enemyA
+    heldByEnemyB = ball.status == BallStatus.enemyB
+
+    if ballNearerMeThanAlly and ballNotNearEnemy:
         collectBall()
-    elif(ball.status == (BallStatus.enemyA or Ballstatus.enemyB)):
-        if( me.distance(ball) < ally.distance(ball)):#if i'm closer to the ball than the ally
+
+    # If the enemy has the ball
+    elif heldByEnemyA or heldByEnemyB:
+        # if i'm closer to the ball than the ally
+        if me.distance(ball) < ally.distance(ball):
             blockPass()
-        else:#defend the goal
+        # otherwise, defend the goal
+        else:
             guardGoal()
-    elif(ball.status == BallStatus.me):#if we have the ball
-        if(lineOfSight(me.currentPoint,opponentGoal) == False):#if we can score a goal,
+
+    # if we have the ball
+    elif ball.status == BallStatus.me:
+        # if we can score a goal,
+        if lineOfSight(me.currentPoint, opponentGoal) == False:
             shootBall()
-        elif(lineOfSight(me.currentPoint,ally.currentPoint) == False):#else, try and pass to ally
+        # else, try and pass to ally
+        elif lineOfSight(me.currentPoint, ally.currentPoint) == False:
             passBall()
 
+if __name__ == '__main__':
+    playBall()
